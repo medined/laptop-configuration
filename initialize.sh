163e5b4f1ab258a3dde00cb424c9ed6a12ac8f0c
Jessica Frazelle <acidburn@docker.com>
2015-12-01 14:21:48 -0500

    add symbolic linking in initialize script

--- initialize.sh
+++ initialize.sh
@@ -1,2 +1,16 @@
-sudo cp etc/fstab /etc/fstab
-sudo cp etc/default/docker /etc/default/docker
+sudo cp /etc/fstab /etc/fstab.original
+sudo cp ./etc/fstab /etc/fstab
+echo "Overwrote /etc/fstab"
+
+sudo cp /etc/default/docker /etc/default/docker.original
+sudo cp ./etc/default/docker /etc/default/docker
+echo "Overwrite /etc/default/docker"
+
+PDIR=$(pwd)
+pushd $HOME > /dev/null
+rm -f .bashrc .dockerfunc
+ln -s $PDIR/.bashrc .bashrc
+echo "Create symbolic link for .bashrc"
+ln -s $PDIR/.dockerfunc .dockerfunc
+echo "Create symbolic link for .dockerfunc"
+popd > /dev/null
